64a4f32806544ade912c56d71bf6a496ed6d8b30
Computador PC <miche@NB-GERAL1.INFO.CORP>
2025-08-25 09:18:39 -0300

    atualizacao projeto

--- src/main/java/br/com/lovizoto/regesc/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/br/com/lovizoto/regesc/exception/handler/CustomEntityResponseHandler.java
@@ -1,6 +1,7 @@
 package br.com.lovizoto.regesc.exception.handler;
 
 import br.com.lovizoto.regesc.exception.ExceptionResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,17 +16,19 @@ import java.util.Date;
 @RestController
 public class CustomEntityResponseHandler {
 
+    // Handler para exceções genéricas (500 Internal Server Error)
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleException(Exception exception, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
-                exception.getMessage(),
+                "Ocorreu um erro interno no servidor.",
                 request.getDescription(false)
         );
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class) //erro que vai dar caso a idade for menor que 18
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public final ResponseEntity<ExceptionResponse> handleValidationException(Exception exception, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
@@ -35,6 +38,7 @@ public class CustomEntityResponseHandler {
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    //Handler para recurso não encontrado (404 Not Found)
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception exception, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
@@ -43,6 +47,17 @@ public class CustomEntityResponseHandler {
                 request.getDescription(false)
         );
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    // Handler para violação de integridade de dados (409 conflict)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public final ResponseEntity<ExceptionResponse> handleDataIntegrityException(Exception exception, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                "Conflito de dados: o recurso já pode existir ou viola um regra de negócio.", //não expõe lógica do banco de dados
+                exception.getMessage()
+        );
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.CONFLICT);
     }
 
 
